6e4c18b3fdd6775230a962f415f870af68781f33
Sean Gallagher <seansg725@gmail.com>
2023-10-26 19:04:43 -0400

    FINISHED!!!

--- PasswordCracker/PasswordCracker/Program.cs
+++ PasswordCracker/PasswordCracker/Program.cs
@@ -46,12 +46,60 @@ namespace PasswordCracker
                 Console.WriteLine(pass);
             }
 
+            // my code :D
+            string[] final = new string[hashedPasswords.Length];
+            Dictionary<string, string> myDictionary = new Dictionary<string, string>();
+            // fills the dictionary with only 5 letters combos that have a hash in passwords_hashed.txt.
+            string[] lowercaseLetters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            string word = "";
+            for (int a = 0; a < 26; a++)
+            {
+                for (int b = 0; b < 26; b++)
+                {
+                    for (int c = 0; c < 26; c++)
+                    {
+                        for (int d = 0; d < 26; d++)
+                        {
+                            for (int e = 0; e < 26; e++)
+                            {
+                                word = lowercaseLetters[a] + lowercaseLetters[b] + lowercaseLetters[c] + lowercaseLetters[d] + lowercaseLetters[e];
+
+                                if (hashedPasswords.Contains(md5(word)))
+                                {
+                                    myDictionary.Add(md5(word), word);
+                                    //print it;
+                                    Console.WriteLine(word + " " + md5(word));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            //loops through my dictionary till it finds the 5 letter combo that matches the hash in passwords_hashed.txt
+            //at the same position, adds to final array.
+
+            for (int i = 0; i < hashedPasswords.Length; i++)
+            {
+                foreach (KeyValuePair<string, string> kvp in myDictionary)
+                {
+                    if (hashedPasswords[i] == kvp.Key)
+                    {
+                        final[i] = kvp.Value;
+                    }
+                }
+            }
+
+            // :P
+
             // Use this method to test if you managed to correctly crack all the passwords
             // Note that hashedPasswords will need to be swapped out with an array the exact
             // same length that contains all the cracked passwords
-            bool passwordsValidated = Validator.ValidateResults(hashedPasswords);
+            bool passwordsValidated = Validator.ValidateResults(final); // i made this check my list instead of the hashed passwords
             
             Console.WriteLine($"\nPasswords successfully cracked: {passwordsValidated}");
+
+            
         }
     }
 }
